c46230fea116e6da8ff359cafb7f5d658dd52a7c
Hasnat Sifat <abulhasnatsifat@gmail.com>
2025-03-22 17:34:44 +0600

    Add files via upload

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Footer from '../Footer/Footer';
 import Nav from '../Header/Nav';
+import Find from '../Find/Find';
 import Homeblog from '../Homeblog/Homeblog';
 import FeaturedPost from '../FeaturedPost/FeaturedPost';
 import NewsletterSignup from '../NewsletterSignup/NewsletterSignup';
@@ -10,12 +11,14 @@ import Testimonials from '../Testimonials/Testimonials';
 const Home = () => {
   return (
 <div>
-  <Nav />   
+  <Nav />
+
   <br/>
   <br/>
   <br/>
    <div className="homeblog">
   <FeaturedPost />
+  <Find />
   <Homeblog/>
   <NewsletterSignup />
   <Testimonials />
